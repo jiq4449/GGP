25e812bb9ec08bc67a246d9075058ed8069d83d9
jiq4449 <jiq4449@g.rit.edu>
2023-02-02 14:59:26 -0500

    Finished tasks 1 & 2 of A2

---
+++ Mesh.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <d3d12.h>
+#include <wrl/client.h>
+#include <string>
+
+#include "Vertex.h"
+
+
+class Mesh
+{
+public:
+	Mesh(Vertex* vertArray, size_t numVerts, unsigned int* indexArray, size_t numIndices);
+	Mesh(const std::wstring& objFile);
+	~Mesh();
+
+	// Getters for mesh data
+	Microsoft::WRL::ComPtr<ID3D12Resource> GetVertexBuffer();
+	Microsoft::WRL::ComPtr<ID3D12Resource> GetIndexBuffer();
+	unsigned int GetIndexCount();
+
+private:
+	// D3D buffers
+	Microsoft::WRL::ComPtr<ID3D12Resource> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D12Resource> indexBuffer;
+
+	D3D12_VERTEX_BUFFER_VIEW vbView;
+	D3D12_INDEX_BUFFER_VIEW ibView;
+
+	// Total indices in this mesh
+	unsigned int numIndices;
+
+	// Helper for creating buffers (in the event we add more constructor overloads)
+	void CreateBuffers(Vertex* vertArray, size_t numVerts, unsigned int* indexArray, size_t numIndices);
+	void CalculateTangents(Vertex* verts, size_t numVerts, unsigned int* indices, size_t numIndices);
+};
+
--- Vertex.h
+++ Vertex.h
@@ -9,6 +9,8 @@
 // --------------------------------------------------------
 struct Vertex
 {
-	DirectX::XMFLOAT3 Position;	    // The local position of the vertex
-	DirectX::XMFLOAT4 Color;        // The color of the vertex
+	DirectX::XMFLOAT3 Position;	    // The position of the vertex
+	DirectX::XMFLOAT2 UV;			// Texture mapping
+	DirectX::XMFLOAT3 Normal;		// Lighting
+	DirectX::XMFLOAT3 Tangent;		// Normal mapping
 };
